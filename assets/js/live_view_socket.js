bd52b306ca7e0050e8fdefac64b1976e0062e8e0
Bohdan Nikolaienko <23bohdan@gmail.com>
2020-01-12 15:29:49 +0200

    Implement chart rendering

--- assets/js/live_view_socket.js
+++ assets/js/live_view_socket.js
@@ -1,5 +1,12 @@
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live", Socket)
+import {BuildChartHook} from "./portfolio_chart"
+
+let Hooks = {}
+
+Hooks.BuildChart = BuildChartHook
+
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks})
+
 liveSocket.connect()
---
+++ assets/js/portfolio_chart.js
@@ -0,0 +1,87 @@
+import 'chartjs-plugin-colorschemes';
+
+export let BuildChartHook = {
+  buildChart() {
+    let portfolio = JSON.parse(this.el.dataset.chart);
+    let Chart = require('chart.js');
+    let ctx = this.el;
+    return new Chart(ctx, {
+      type: 'line',
+      data: chart_data(portfolio),
+      options: {
+        title: {
+          display: true,
+          text: "Portfolio Performance Chart"
+        },
+        scales: {
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: "Balance, $"
+              }
+            }],
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'month'
+                }
+            }]
+        },
+        plugins: {
+            colorschemes: {
+                scheme: 'brewer.Paired12'
+            }
+        }
+      }
+    });
+  },
+
+  mount() {
+    this.chart = this.buildChart();
+  },
+
+  updated() {
+    if (typeof this.chart !== 'undefined') {
+        this.chart.destroy();
+        this.chart = this.buildChart();
+    } else {
+      this.chart = this.buildChart();
+    }
+  }
+}
+
+function chart_data(json) {
+  let array_data = Object.keys(json).map(key => [new Date(key), json[key]]);
+
+  if (array_data.length == 0) return {};
+
+  array_data.sort((a, b) => a[0] - b[0]);
+
+  let stocks = Object.keys(array_data[0][1].assets);
+
+  let total_data = []
+  let labels = []
+  array_data.forEach(function(data) {
+    labels.push(data[0])
+    total_data.push(data[1].balance / 100)
+  });
+
+  let stocks_dataset = stocks.map(function(ticker) {
+    return {
+      data: array_data.map(v => (v[1].assets[ticker].price * v[1].assets[ticker].amount) / 100),
+      label: ticker,
+      fill: false
+    }
+  });
+
+  stocks_dataset.push({
+    data: total_data,
+    label: "Total balance",
+    fill: false
+  })
+
+  return {
+    labels: labels,
+    datasets: stocks_dataset
+  }
+};
