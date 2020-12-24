70abe5569b74aed5b0e894a1b5bfe4606078f989
Bohdan Nikolaienko <23bohdan@gmail.com>
2020-12-24 13:02:21 +0200

    Fix config

--- config/config.exs
+++ config/config.exs
@@ -23,10 +23,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
-
 config :portfolio_performance,
   marketstack_url: "https://api.marketstack.com/v1/",
   marketstack_access_key: System.get_env("MARKETSTACK_ACCESS_KEY")
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
